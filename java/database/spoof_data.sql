f04c4cd88244af1c376732c5491741feec002ab8
Tyler Dix <tylerdix8@gmail.com>
2023-12-05 15:21:20 -0500

    iteration

--- java/database/spoof_data.sql
+++ java/database/spoof_data.sql
@@ -5,16 +5,26 @@
 BEGIN TRANSACTION;
 
 INSERT INTO users(username, password_hash, role)
-VALUES ('CHAAAACHIIIII', 'er34289yh2uhg', 'ROLE_DOCTOR');
+VALUES ('CHAAAACHIIIII', 'er34289yh2uhg', 'ROLE_DOCTOR'),
+       ('Joni', 'joniiiiiii', 'ROLE_USER'),
+       ('PickleRick', 'solenya', 'ROLE_DOCTOR'),
+       ('BethSanchez', 'horsies4lyfe', 'ROLE_DOCTOR');
 
 INSERT INTO specialty(specialty_name)
-VALUES ('Endocrinologist');
+VALUES ('Endocrinologist'),
+       ('Equine Surgeon');
 
 INSERT INTO person(user_id, first_name, last_name, email, date_of_birth)
-VALUES (1, 'Chachi', 'TheCat', 'chachicat@meowmeow.com', '2003-01-03');
+VALUES (1, 'Chachi', 'TheCat', 'chachicat@meowmeow.com', '2003-01-03'),
+       (2, 'Joni', 'LovesChachi', 'joni@chachicat.net', '2005-01-23'),
+       (3, 'Rick', 'Sanchez', 'picklerick@interdimentionalmail.universe', '1968-12-23'),
+       (4, 'Beth', 'Sanchez', 'beth@interdimentionalmail.universe', '1989-12-01');
 
 INSERT INTO doctor(person_id, specialty_id, is_primary_care)
-VALUES (1, 1, True);
+VALUES (1, 1, True),
+       (2, 2, True),
+       (3, 1, True),
+       (4, 2, True);
 
 INSERT INTO address(street_address, city, state_abbreviation, zip_code)
 VALUES (420, 'CatTown', 'CT', 12345);
