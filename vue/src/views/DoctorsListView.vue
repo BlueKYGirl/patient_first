dcb12d1668ae32868f12197d79bb840f2707b85b
Toni Moore <toni.denise.moore@gmail.com>
2023-12-05 19:48:16 -0500

    More changes

--- vue/src/views/DoctorsListView.vue
+++ vue/src/views/DoctorsListView.vue
@@ -5,6 +5,22 @@
 </template>
 
 <script>
+import DoctorsList from '../components/DoctorsList.vue';
+import doctorsService from '../services/DoctorsService.js';
+
+export default {
+    components: {
+        DoctorsList,
+    },
+    data() {
+        return {
+
+        }
+    },
+    methods: {
+        
+    }
+}
 
 </script>
 
