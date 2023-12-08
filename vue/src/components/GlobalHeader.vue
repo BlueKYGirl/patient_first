7243771120c29a9f5f5e783d5cbf79d25298bcb0
Toni Moore <toni.denise.moore@gmail.com>
2023-12-07 21:08:31 -0500

    Improved header

--- vue/src/components/GlobalHeader.vue
+++ vue/src/components/GlobalHeader.vue
@@ -1,15 +1,42 @@
 <template>
   <body>
-    <div id="nav">
-      <img class="logo" src="../assets/smallTransparent.png" style="float: left; margin-right: 15px;"/>
-    <router-link v-bind:to="{ name: 'home' }">Home</router-link>&nbsp;&nbsp; |
-    <router-link v-bind:to="{ name: 'logout' }" v-if="this.$store.state.token != ''">Logout</router-link>
+    <div id="header">
+      <img class="logo" src="../assets/smallTransparent.png" />
+      <div class="nav">
+
+    <router-link v-bind:to="{ name: 'home' }">Home</router-link>&nbsp;&nbsp; |&nbsp;&nbsp;
+    <router-link v-bind:to="{ name: 'logout' }" v-if="this.$store.state.token != ''"> Logout</router-link></div>
+   
   </div>
+  
 </body>
 </template>
 
 <style>
-.logo {
+
+
+#header {
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  margin-bottom: 2%;
+}
+.nav {
+  display: flex;
+  flex-direction: row;
+  margin-top: 1%;
+  font-size: x-large;
+  
+}
+a {
+  text-decoration: none;
+  color: #614BC3;
+}
+a.active {
+  color: #33BBC5;
+}
+
+/*.logo {
   display: block;
   width: 10%;
   text-align: left;
@@ -17,9 +44,9 @@
   margin-bottom: 5px;
   position: absolute;
 
-}
-#nav {
+}*/
+/*#nav {
   text-align: right;
-}
+}*/
 </style>
 
