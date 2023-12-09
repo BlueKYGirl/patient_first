815764a0d20752a4367070c12cbb7d71657cda4e
Shianne Hollins <srhollins4@gmail.com>
2023-12-09 00:34:20 -0500

    SHOLLINS: doctorList/officeDetails/Login update

--- vue/src/views/HomeView.vue
+++ vue/src/views/HomeView.vue
@@ -5,17 +5,29 @@
     
   </div>
 </body>
+<div class="footer"> 
+  <global-footer/>
+</div>
 </template>
 
 <script>
+
+
+//*********************************************************************
+//ADDED THE FOOTER ON HERE FOR YOU, IF YOU DON'T NEED IT, JUST DELETE :) -Shianne
+//**********************************************************************
+
+
 import GlobalHeader from '../components/GlobalHeader.vue';
+import GlobalFooter from '../components/GlobalFooter.vue';
 
 //import FileUpload from '../components/FileUpload.vue';
 //import ImageViewer from '../components/ImageViewer.vue';
 
 export default {
   components: {
-    GlobalHeader
+    GlobalHeader,
+    GlobalFooter
     
   }
 };
