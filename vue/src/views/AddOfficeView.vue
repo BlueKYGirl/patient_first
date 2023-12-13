45ed5317abba08ef8dcaf54964912b137cc36e0c
Tyler Dix <tylerdix8@gmail.com>
2023-12-13 16:03:06 -0500

    TDIX: 12/13/23 - Add / Update Office Page Wiring Completed...ish

--- vue/src/views/AddOfficeView.vue
+++ vue/src/views/AddOfficeView.vue
@@ -6,6 +6,7 @@
         </div>
     <div class="add-new-office">
     <add-new-office/>
+    <!-- <edit-office-info/> -->
         </div>
             <div class="footer">
                 <global-footer />
@@ -18,12 +19,14 @@
     import GlobalFooter from '../components/GlobalFooter.vue';
     import AddNewOffice from '../components/AddNewOffice.vue';
     import officesService from '../services/OfficesService';
+    // import EditOfficeInfo from '../components/EditOfficeInfo.vue';
 
     export default {
         components: {
             GlobalHeader,
             GlobalFooter,
-            AddNewOffice
+            AddNewOffice,
+            // EditOfficeInfo
         },
         methods: {
             createOffices() {
