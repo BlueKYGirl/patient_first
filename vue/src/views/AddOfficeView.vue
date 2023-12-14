7f12290793fe28af6081e15dac458ddcd94daf44
Tyler Dix <tylerdix8@gmail.com>
2023-12-14 08:57:15 -0500

    Clean up

--- vue/src/views/AddOfficeView.vue
+++ vue/src/views/AddOfficeView.vue
@@ -6,7 +6,6 @@
         </div>
     <div class="add-new-office">
     <add-new-office/>
-    <!-- <edit-office-info/> -->
         </div>
             <div class="footer">
                 <global-footer />
@@ -19,14 +18,12 @@
     import GlobalFooter from '../components/GlobalFooter.vue';
     import AddNewOffice from '../components/AddNewOffice.vue';
     import officesService from '../services/OfficesService';
-    // import EditOfficeInfo from '../components/EditOfficeInfo.vue';
 
     export default {
         components: {
             GlobalHeader,
             GlobalFooter,
             AddNewOffice,
-            // EditOfficeInfo
         },
         methods: {
             createOffices() {
